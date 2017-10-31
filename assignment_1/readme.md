0c3111be36bf70da96b61e28b4eaec4cd75f0d8f
William Eriksson <willeeh@hotmail.com>
2017-10-31 17:25:18 +0100

    First assignment v1.0

---
+++ assignment_1/readme.md
@@ -0,0 +1,3 @@
+# Description
+First assignment of the course, a simple guessing game from the Rust tutorials. Extended with Error message on bad input and also with number of attempts counter.
+Simply run it with cargo run and try to find the right number by entering guesses of numbers.
---
+++ assignment_1/src/main.rs
@@ -0,0 +1,41 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut attempts = 0;
+
+    loop {
+        attempts += 1;
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(err) =>  {
+                println!("Error: {:?}", err);
+                continue;
+            },
+        };
+
+        println!("You guessed: {}, nr of attempts: {}", guess, attempts);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
