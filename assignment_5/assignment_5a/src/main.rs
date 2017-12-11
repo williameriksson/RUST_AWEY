62291b19bc69289300630063ca86a98cc9557851
William Eriksson <willeeh@hotmail.com>
2017-12-11 21:23:22 +0100

    Blinking led and logging task done

--- assignment_5/assignment_5a/src/main.rs
+++ assignment_5/assignment_5a/src/main.rs
@@ -2,16 +2,16 @@
 //#![deny(unsafe_code)]
 #![feature(proc_macro)]
 
-
-extern crate cortex_m_semihosting;
+//extern crate cortex_m_semihosting;
 extern crate stm32f103xx;
 extern crate cortex_m_rtfm as rtfm;
 extern crate cortex_m;
 
-use core::fmt::Write;
+use cortex_m::{iprint, iprintln};
+//use core::fmt::Write;
 use cortex_m::peripheral::SystClkSource;
-use cortex_m_semihosting::hio;
-use stm32f103xx::DWT;
+// use cortex_m_semihosting::hio;
+//use stm32f103xx::DWT;
 use rtfm::{app, Threshold, Resource};
 
 const FREQUENCY: u32 = 1; // Hz
@@ -20,32 +20,25 @@ app! {
     device: stm32f103xx,
 
     resources: {
-        static IDLE_COUNTER: u64 = 0;
-        static BUSY_COUNTER: u64 = 0;
+        static IDLE_COUNTER: u32 = 0;
+        static BUSY_COUNTER: u32 = 0;
        // static BLINK_PERIOD: u16 = 100;
-       // static CPU_USAGE: u8 = 0;
     },
 
     idle: {
-        resources: [IDLE_COUNTER, BUSY_COUNTER, GPIOC],
+        resources: [IDLE_COUNTER, BUSY_COUNTER, GPIOC, ITM, DWT],
     },
 
-    // Both SYS_TICK and TIM2 have access to the `COUNTER` data
     tasks: {
         SYS_TICK: {
             path: logging,
-            resources: [DWT, IDLE_COUNTER, BUSY_COUNTER],
+            resources: [DWT, IDLE_COUNTER, BUSY_COUNTER, ITM],
         },
 
         TIM2: {
             path: blink_led,
             resources: [TIM2, GPIOC],
         },
-
-       // BLINK: {
-       //     path: blink,
-       //     resources: [BLINK_PERIOD],
-       // },
 
        // USART1: {
        //     path: usart2,
@@ -71,8 +64,8 @@ fn init(p: ::init::Peripherals, _r: ::init::Resources) {
 
     p.RCC.apb1enr.modify(|_, w| w.tim2en().set_bit()); // Enable TIM2 peripheral clock
    
-    let prescaler = 16000 - 1; // 16 Mhz / 16000 = 1kHz
-    p.TIM2.psc.write(|w| unsafe{w.psc().bits(prescaler)});
+    let prescaler = 8000 - 1; // 8 Mhz / 8000 = 1kHz
+    p.TIM2.psc.write(|w| w.psc().bits(prescaler));
 
     // Set the auto reload register
     let arr_value = 1000;
@@ -88,39 +81,63 @@ fn init(p: ::init::Peripherals, _r: ::init::Resources) {
         p.DWT.sleepcnt.write(0);
     }
     p.DWT.enable_cycle_counter();
+
+    iprintln!(&p.ITM.stim[0], "init done");
 }
 
 fn idle(t: &mut Threshold, mut r: ::idle::Resources) -> ! {
-    let mut stdout = hio::hstdout().unwrap();
-  
-   // r.GPIOC.odr.write(|w| w.odr13().bit(false));
+ 
+    let mut prev_idle_end = 0;
     loop {
-        r.IDLE_COUNTER.claim_mut(t, |v, _t| **v = 0);
-
-        rtfm::wfi();
-        //let led_on = r.GPIOC.odr.read().odr13().bit();
-        //writeln!(stdout, "{}", led_on);
-        //r.GPIOC.odr.write(|w| w.odr13().bit(!led_on));
-       
+        cortex_m::interrupt::disable();
         
-        //r.COUNTER.claim_mut(t, |v, _t| **v = 0); 
+        let mut idle_start = 0;
+        r.DWT.claim(t, |v, _t| idle_start = v.cyccnt.read());
+        
+        rtfm::wfi();
+         
+        let mut idle_end = 0;
+        r.DWT.claim(t, |v, _t| idle_end = v.cyccnt.read());
+
+        let time_idle = idle_end.wrapping_sub(idle_start);
+        let time_busy = idle_start.wrapping_sub(prev_idle_end);
+
+        r.IDLE_COUNTER.claim_mut(t, |v, _t| **v = time_idle); 
+        r.BUSY_COUNTER.claim_mut(t, |v, _t| **v = time_busy);
+        
+        prev_idle_end = idle_end;
+
+        unsafe{cortex_m::interrupt::enable();}
+
     }
 }
 
 fn logging(t: &mut Threshold, r: SYS_TICK::Resources) {
     
-    let mut stdout = hio::hstdout().unwrap();
-    let cyc = r.DWT.cyccnt.read();
-    let slp = r.DWT.sleepcnt.read();
-    // writeln!(stdout, "cyc: {}", cyc);
-    // writeln!(stdout, "slp: {}", slp);
-    //r.COUNTER.claim_mut(t, |v, _t| **v = 0); 
+    let mut percent: u32 = 0;
+
+    let mut time_idle = 0;
+    let mut time_busy = 0;
+
+    r.IDLE_COUNTER.claim(t, |v, _t| time_idle = **v);
+    r.BUSY_COUNTER.claim(t, |v, _t| time_busy = **v);
+
+    let total_time = time_idle.wrapping_add(time_busy); 
+    if total_time > 0 {
+        percent = time_busy.wrapping_mul(100).wrapping_div(total_time);
+    }
+  
+    if percent < 50 {
+        for _ in 0..8000 {};
+    } else {
+        for _ in 0..3000 {};
+    }
+    
+    iprintln!(&r.ITM.stim[0],"busy: {}, idle: {}, percent: {}", time_busy, time_idle, percent);
 }
 
 fn blink_led(t: &mut Threshold, r: TIM2::Resources) {
      
-    //let mut led_on = false;
-   
     r.TIM2.claim_mut(t, |tim2, _t| {
         tim2.sr.write(|w| w.uif().clear_bit()); 
     
@@ -130,12 +147,4 @@ fn blink_led(t: &mut Threshold, r: TIM2::Resources) {
         
         gpioc.odr.write(|w| w.odr13().bit( !gpioc.odr.read().odr13().bit() ));
     });
-    //r.COUNTER.claim_mut(t, |v, _t| **v = 0); 
 }
-//fn main() {
-
-    // (*DWT.get()).enable_cycle_counter();
-   // (*DWT.get()).cyccnt.write(0);
-   // cycle_count = (*DWT.get()).cyccnt.read();
-    
-//}
